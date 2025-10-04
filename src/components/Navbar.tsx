fe206f80511b271ac7b68107d366521fb8251ca4
hugo <hugo.nahmias@icloud.com>
2025-10-04 12:46:41 +0200

    fix: ajuster le style et la structure de la barre de navigation pour une meilleure présentation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -31,13 +31,15 @@ const Navbar = () => {
 
 		return (
 			<nav
-				className={`w-full py-4 px-8 flex items-center justify-between fixed top-0 left-0 z-50 transition-transform duration-500 ${show ? "translate-y-0" : "-translate-y-full"}`}
+				className={`w-full py-4 px-4 md:px-8 flex items-center justify-between fixed top-0 left-0 z-50 transition-transform duration-500 ${show ? "translate-y-0" : "-translate-y-full"}`}
 				style={{ background: "linear-gradient(180deg, #faf6e7 20%, transparent 100%)" }}
 			>
 			<div className="flex items-center">
-				<img src={Logo} alt="Logo" className="h-16 w-auto " />
+				<img src={Logo} alt="Logo" className="h-12 md:h-16 w-auto" />
 			</div>
-			<ul className="flex-1 flex justify-center gap-24">
+			
+			{/* Navigation desktop */}
+			<ul className="hidden md:flex flex-1 justify-center gap-24">
 				{navLinks.map((link) => (
 					<li key={link.href}>
 						<a
@@ -49,7 +51,22 @@ const Navbar = () => {
 					</li>
 				))}
 			</ul>
-			<div className="w-12" /> {/* Pour équilibrer l'espace à droite */}
+			
+			{/* Navigation mobile */}
+			<ul className="flex md:hidden gap-6">
+				{navLinks.map((link) => (
+					<li key={link.href}>
+						<a
+							href={link.href}
+							className="text-[#ff4300] text-sm font-light hover:underline transition"
+						>
+							{t(link.label)}
+						</a>
+					</li>
+				))}
+			</ul>
+			
+			<div className="w-4 md:w-12" /> {/* Pour équilibrer l'espace à droite */}
 		</nav>
 	);
 };
